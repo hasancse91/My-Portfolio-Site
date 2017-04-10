bb07ebca5fefa58740330d9288cd88d4bc7e4949
hasan <hasan_cse91@yahoo.com>
2017-04-10 17:55:43 +0600

    information updated

--- Portfolio-SourceCode/mail/contact_me.php
+++ Portfolio-SourceCode/mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'hasan.cse91@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'YOUR EMAIL ADDRESS';
 $email_subject = "Contact Form Portfolio site:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: ".$email_address."\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
